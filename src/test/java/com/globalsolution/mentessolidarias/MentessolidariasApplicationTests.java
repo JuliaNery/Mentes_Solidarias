c892084c97391df1bfe521026508049008e4b912
Julia Nery <julianerysinigalia@gmail.com>
2023-11-15 21:38:50 -0300

    Subindo projeto inicial

---
+++ src/test/java/com/globalsolution/mentessolidarias/MentessolidariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalsolution.mentessolidarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentessolidariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
